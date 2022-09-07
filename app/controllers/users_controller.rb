72067cd132f8953ab3241224a55e84624694aeeb
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-09-07 01:24:14 -0400

    commit tree 4cb2a019d8e701d2c0f0d30aefc2d0f3c715b363

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def index
+        render json: User.all
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user.to_json(methods: [:items])
+    end
 end
